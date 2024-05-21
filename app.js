b88f841378e2ed77d17eb2fccbb3092241159bff
Jack Davenport <jckwdvnprt@gmail.com>
2024-05-21 11:58:41 +0100

    Added mongoDB connection and basic Schema building

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 const app = express();
-
 
 mongoose.set("strictQuery", false);
 
 //connect to mongoose
-mongoose.connect('mongodb+srv://admin:tetriplan@cluster0.spo41rp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-const database = client.db();
-const tetriTask = database.use('Cluster0');
+mongoose
+  .connect(
+    "mongodb+srv://admin:tetriplan@cluster0.spo41rp.mongodb.net/tetriplan-tasks"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
 
 //schema
-
-const taskSchema = new Schema({
-  taskID: {
-    type: Number,
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+});
+
+const taskSchema = new mongoose.Schema({
   userID: {
     type: Number,
     required: true,
@@ -42,7 +54,7 @@ const taskSchema = new Schema({
     default: () => Date.now(),
     immutable: true,
   },
-  startTime:{
+  startTime: {
     type: String,
     required: true,
   },
@@ -67,25 +79,26 @@ const taskSchema = new Schema({
     type: String,
     required: true,
     lowercase: true,
-  }
+  },
 });
 
 //error handling
-app.all('*', (request, response, next) => {
-    response.status(404).send({ message: 'endpoint not found' });
-  })
-  
+app.all("*", (request, response, next) => {
+  response.status(404).send({ message: "endpoint not found" });
+});
+
 app.use((err, request, response, next) => {
-    if (err.status && err.message)
-      response.status(err.status).send({ message: err.message });
-    next(err);
-  });
-  
-  app.use((err, request, response, next) => {
-    response.status(500).send({ message: 'internal server error'});
-  });
+  if (err.status && err.message)
+    response.status(err.status).send({ message: err.message });
+  next(err);
+});
 
+app.use((err, request, response, next) => {
+  response.status(500).send({ message: "internal server error" });
+});
 
-const Task = model('tetriplan-tasks', taskSchema);
+const Task = model("Task", taskSchema, "tetriplan-tasks");
+const User = model("User", userSchema, "tetriplan-users");
 
 module.exports = app;
+module.exports = { Task, User };
